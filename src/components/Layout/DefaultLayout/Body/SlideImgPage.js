1db70167dd276e9cafc7b668dc7b573a57949b59
TranHoangMy <thmy0401@gmail.com>
2025-02-01 00:47:20 +0700

    UpdateCallAPi

--- src/components/Layout/DefaultLayout/Body/SlideImgPage.js
+++ src/components/Layout/DefaultLayout/Body/SlideImgPage.js
@@ -8,7 +8,7 @@ function SlideImgPage({ cx }) {
     const [youtubelink, setYoutubeLink] = useState(null)
     useEffect(() => {
         axios.all([
-            axios.get('http://localhost:8080/api/v12/showimgbody'),
+            axios.get(`${process.env.REACT_APP_CALL_API}/api/v12/showimgbody`),
           ])
             .then(axios.spread((imgBody, ) => { 
               setBodyimg(imgBody.data.data)
@@ -25,11 +25,11 @@ function SlideImgPage({ cx }) {
                     <iframe width="100%" height="100%" src={(bodyimg.length !== 0 ? (youtubelink) : (null))} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
                 <div className={cx('SlideImgPage_Container_contain_img')}>
-                    <img className={cx('SlideImgPage_Container_contain_img-item')} src={(bodyimg.length !== 0 ? (`http://localhost:8080/api/v12/bodyimg/${bodyimg[0].Img}`) : (null))} />
+                    <img className={cx('SlideImgPage_Container_contain_img-item')} src={(bodyimg.length !== 0 ? (`${process.env.REACT_APP_CALL_API}/api/v12/bodyimg/${bodyimg[0].Img}`) : (null))} />
                 </div>
                 <div className={cx('SlideImgPage_Container_contain_img-mini')}>
-                    <img src={(bodyimg.length !== 0 ? (`http://localhost:8080/api/v12/bodyimg/${bodyimg[1].Img}`) : (null))} className={cx('SlideImgPage_Container_contain_img-mini-left')} />
-                    <img src={(bodyimg.length !== 0 ? (`http://localhost:8080/api/v12/bodyimg/${bodyimg[2].Img}`) : (null))} className={cx('SlideImgPage_Container_contain_img-mini-right')} />
+                    <img src={(bodyimg.length !== 0 ? (`${process.env.REACT_APP_CALL_API}/api/v12/bodyimg/${bodyimg[1].Img}`) : (null))} className={cx('SlideImgPage_Container_contain_img-mini-left')} />
+                    <img src={(bodyimg.length !== 0 ? (`${process.env.REACT_APP_CALL_API}/api/v12/bodyimg/${bodyimg[2].Img}`) : (null))} className={cx('SlideImgPage_Container_contain_img-mini-right')} />
                 </div>
             </div>
         </div>
